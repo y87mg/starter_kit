acd6501658f8ddd596cc113d173f2b0866372bb3
Yury <forwebwork2018@gmail.com>
2021-05-06 12:28:35 +0400

    fix eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: `module`,
+  },
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+  extends: `eslint:recommended`,
+  rules: {
+    "quotes": [`error`, `backtick`],
+    "semi": [`error`, `always`],
+    "comma-dangle": [`error`, {
+      'arrays': `always-multiline`,
+      'objects': `always-multiline`,
+      'functions': `always-multiline`,
+    }],
   },
-  rules: {},
 };
